56fa7487e2d5f52c0040bede7cce64497d695ebd
Jeremy DeJournett <jcdejournett@gmail.com>
2022-06-29 17:58:35 -0400

    Used a slightly less verbose naming scheme, adjusting types
    relative_timestamp should be FLOAT (or maybe DECIMAL?)
    
    TODO: convert FLOAT to DECIMAL? use an ORM?

--- table_creation.sql
+++ table_creation.sql
@@ -2,51 +2,51 @@ CREATE TYPE sensor_source AS ENUM ('id 0', 'id 1', 'aggregate sensor');
 CREATE TYPE concentration_units_types AS ENUM ('U/mL', 'g/100mL', '%wt');
 CREATE TYPE log_types AS ENUM ('data log', 'error log');
 
-CREATE TABLE patient_table (
+CREATE TABLE patient (
   id SERIAL,
   mr_number TEXT,
   PRIMARY KEY(id)
 );
 
-CREATE TABLE session_table (
+CREATE TABLE session (
   id SERIAL,
   weight_kg FLOAT,
   glucose_units FLOAT,
   control_range_mg_dl TEXT,
   initial_glucose_mg_dl FLOAT,
   PRIMARY KEY(id),
-  patient_id INTEGER REFERENCES patient_table(id)
+  patient_id INTEGER REFERENCES patient(id)
 );
 
-CREATE TABLE log_table (
+CREATE TABLE log (
   id SERIAL,
   original_log_name TEXT,
   log_type log_types,
   PRIMARY KEY(id),
-  session_id INTEGER REFERENCES session_table(id)
+  session_id INTEGER REFERENCES session(id)
 );
 
-CREATE TABLE glucose_table (
+CREATE TABLE glucose (
   id SERIAL,
   timestamp TIMESTAMP,
-  relative_timestamp TIMESTAMP,
+  relative_timestamp FLOAT,
   source sensor_source,
   value_mg_dl FLOAT,
   PRIMARY KEY(id),
-  patient_id INTEGER REFERENCES patient_table(id),
-  log_source INTEGER REFERENCES log_table(id)
+  patient_id INTEGER REFERENCES patient(id),
+  log_source INTEGER REFERENCES log(id)
 );
 
-CREATE TABLE pump_rate_table (
+CREATE TABLE pump_rate (
   id SERIAL,
   timestamp TIMESTAMP,
-  relative_timestamp TIMESTAMP,
+  relative_timestamp FLOAT,
   rate_ml_hr FLOAT,
   substance TEXT,
   concentration FLOAT,
   concentration_units concentration_units_types,
   PRIMARY KEY(id),
-  patient_id INTEGER REFERENCES patient_table(id),
-  log_source INTEGER REFERENCES log_table(id)
+  patient_id INTEGER REFERENCES patient(id),
+  log_source INTEGER REFERENCES log(id)
 );
 
