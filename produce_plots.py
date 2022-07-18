94623fb1f9deb168ca464ecd99ddf342d61593e0
Jeremy DeJournett <jcdejournett@gmail.com>
2022-07-17 22:45:26 -0400

    Added title, legend

--- produce_plots.py
+++ produce_plots.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import os
 import matplotlib.pyplot as plt
 from imt_analysis import get_data_log_df_setup
 import pandas as pd
@@ -8,10 +9,28 @@ import numpy as np
 # Trying to make it pretty
 import seaborn
 
+# This is hacky way to do this, but it should work well enough
+subject_id_lookup = {
+        "EM03" : "Subject 1",
+        "EM02" : "Subject 2",
+}
+
+
+def datalog_to_datestr(filename):
+    logname = os.path.basename(filename)
+    logname = logname.rstrip(".csv")
+    logname = logname.lstrip("IMT_data_log_")
+    year, month, day, _ = logname.split("_")
+    return f"{year}/{month}/{day}"
+
+
 def create_plots(config):
     glucose_df, setup_info = get_data_log_df_setup(config.datalog)
 
     patient_id = setup_info["Subject Study ID"]
+    subject_id = subject_id_lookup[patient_id] # yes, the naming is confusing. If this errors, you need to update for more patients
+
+    study_date = datalog_to_datestr(config.datalog)
 
     pump_df = pd.read_csv(config.ratelog)
 
@@ -53,7 +72,15 @@ def create_plots(config):
     ins_axis = gluc_axis.twinx()
     dex_axis = gluc_axis.twinx()
 
+    # Remove excessive whitespace
     gluc_axis.set_xlim(-5, glucose_df["rel_time_min"].max() + 20)
+
+    # Force the glucose axis to standard limits
+    gluc_axis.set_ylim(60, 260)
+
+    # Make it 7.5in in x dimension, to fit in margins
+#    fig.set_size_inches(7.5, 4.0)
+#    fig.set_dpi(120) # works for my monitor only!
     
     # Label the axes
     gluc_axis.set_ylabel("Glucose (mg/dL)")
@@ -65,6 +92,8 @@ def create_plots(config):
     # They had a bug, though, its spines["right"]
     dex_axis.spines["right"].set_position(("axes", 1.08))
 
+    # Add a title
+    plt.title(f"{subject_id} - {study_date}")
 
     # Now plot the data, using the colors from the LabVIEW plot
     gluc_axis.plot(glucose_df["rel_time_min"], glucose_df["Glucose (mg/dL)"]
@@ -85,16 +114,25 @@ def create_plots(config):
     )
 
     # Insulin
-    ins_axis.plot(insulin["rel_time_min"], insulin["rate1_per_kg"]
+    ins_axis.step(insulin["rel_time_min"], insulin["rate1_per_kg"]
             , color="#FF4242"
             , label="Insulin Rate (U/kg/hr)"
+            , where="post"
     )
 
     # Dextrose (including boluses)
-    dex_axis.plot(dextrose["rel_time_min"], dextrose["rate1_per_kg"]
+    dex_axis.step(dextrose["rel_time_min"], dextrose["rate1_per_kg"]
             , color="#0EFF00"
             , label="Dextrose Rate (mg/kg/min)"
+            , where="post"
     )
+    
+    # Combine the legends together into one
+    h1, l1 = gluc_axis.get_legend_handles_labels()
+    h2, l2 = dex_axis.get_legend_handles_labels()
+    h3, l3 = ins_axis.get_legend_handles_labels()
+
+    plt.legend(h1+h2+h3, l1+l2+l3, loc='best')
 
     plt.show()
 
